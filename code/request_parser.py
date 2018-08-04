d080aba5eba63d94851fc4444151c5b3c2508afd
naschorr <naschorr@gmail.com>
2018-08-04 18:10:15 +0000

    Updated request_parser to handle string and byte arrays in the request body

--- code/request_parser.py
+++ code/request_parser.py
@@ -5,7 +5,7 @@ from nest_state_event import NestStateEvent
 
 class NestAwayRequestParser:
     def parse(self, body):
-        request_json = json.loads(body)
+        request_json = json.loads(body.decode('utf-8'))
 
         ## Trim out any unwanted properties
         state = NestStateEvent(request_json.get('user'), request_json.get('away_state'))
