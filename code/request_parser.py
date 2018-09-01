453c64441f9dd06d19a60e2da98fc88785adb4dd
Nick Schorr <naschorr@gmail.com>
2018-09-01 13:30:04 +0000

    Added try catch block to handle json request parsing
    - If it can't parse the json string, convert it to utf-8 and try again

--- code/request_parser.py
+++ code/request_parser.py
@@ -5,7 +5,10 @@ from nest_state_event import NestStateEvent
 
 class NestAwayRequestParser:
     def parse(self, body):
-        request_json = json.loads(body.decode('utf-8'))
+        try:
+            request_json = json.loads(body)
+        except TypeError:
+            request_json = json.loads(body.decode('utf-8'))
 
         ## Trim out any unwanted properties
         state = NestStateEvent(request_json.get('user'), request_json.get('away_state'))
